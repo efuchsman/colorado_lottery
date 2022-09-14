991861cd82adc598ad9ea3602cc428f438b69b60
Eli Fuchsman <efuchsman@gmail.com>
2022-09-13 21:41:33 -0600

    Initialize class and create test for readable attributes

---
+++ lib/contestant.rb
@@ -0,0 +1,19 @@
+class Contestant
+
+  attr_reader :first_name, :last_name, :age, :state_of_residence, :spending_money, :full_name, :game_interests
+
+  def initialize(attributes)
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
+    @age =  attributes[:age]
+    @state_of_residence = attributes[:state_of_residence]
+    @spending_money = attributes[:pending_money]
+    @full_name = "#{@first_name} #{@last_name}"
+    @game_interests = []
+  end
+
+  def out_of_state?
+    @state_of_residence != "CO"
+  end
+
+end
---
+++ test/contestant_test.rb
@@ -0,0 +1,39 @@
+require 'rspec'
+require './lib/game'
+require './lib/contestant'
+
+RSpec.describe Contestant do
+  it 'exists' do
+
+alexander = Contestant.new(
+  {
+    first_name: 'Alexander',
+    last_name: 'Aigiades',
+    age: 28,
+    state_of_residence: 'CO',
+    pending_money: 10
+    })
+
+    expect(alexander).to be_an_instance_of(Contestant)
+  end
+
+  it 'has readable attributes' do
+    alexander = Contestant.new(
+      {
+        first_name: 'Alexander',
+        last_name: 'Aigiades',
+        age: 28,
+        state_of_residence: 'CO',
+        pending_money: 10
+        })
+
+
+    expect(alexander.full_name).to eq("Alexander Aigiades")
+    expect(alexander.age).to eq(28)
+    expect(alexander.state_of_residence).to eq("CO")
+    expect(alexander.spending_money).to eq(10)
+    expect(alexander.out_of_state?).to be false
+    expect(alexander.game_interests).to eq([])
+
+  end
+end
